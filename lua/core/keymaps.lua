d7dd33f48d762c858f86c81cd5c8f524861d0c8f
Connor Cummings <94203733+WordlessSafe1@users.noreply.github.com>
2024-02-27 18:35:34 -0600

    Add keybuffer cycle, remove jj bind

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,7 +15,26 @@ vim.opt.shiftround = true
 vim.opt.expandtab = false
 
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
 
-vim.keymap.set('n', '<leader>b', ':ls<CR>:b<Space>'); -- Show buffers
+vim.keymap.set('n', '<leader>b', ':ls<CR>:b'); -- Show buffers
+-- vim.keymap.set({'n', 'v'}, '<Leader>v', ':let @x=@" | let @"=@a | let @a=@b | let @b=@x<CR>');
+
+local KeyBufferState = 1; -- 1..3 - ", a, b
+local KeyBufferStates = { '@\\"  @a @b', '@a  @b @\\"', '@b  @\\" @a' };
+function CycleBuffers(amount)
+	local command;
+	if amount < 0 then
+		command = ':let @x=@b | let @b=@a | let @a=@" | let @"=@x';
+	else
+		command = ':let @x=@" | let @"=@a | let @a=@b | let @b=@x';
+	end
+	for _ = 1, math.abs(amount), 1 do
+		vim.cmd(command);
+	end
+	KeyBufferState = ((KeyBufferState - 1 + amount) % 3) + 1;
+	vim.cmd('echo "' .. KeyBufferStates[KeyBufferState] .. '"');
+end
+
+vim.keymap.set({'n', 'v'}, '<Leader>v', function() CycleBuffers( 1) end, { noremap= true });
+vim.keymap.set({'n', 'v'}, '<Leader>V', function() CycleBuffers(-1) end);
 
